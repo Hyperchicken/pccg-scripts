035cf6bc36bed5157f4b58cd2febb9003bfc9a16
Hyperchicken <petar@hyperchicken.com>
2016-10-03 22:18:14 +1100

    added ra page script

---
+++ RA Page.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        RA Page
+// @namespace   http://hyperchicken.com
+// @include     file:///C:/Users/petar/Desktop/Programming/pccg-scripts/samples/Warranties%20-%20PC%20Case%20Gear.htm
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+document.title = 'RA ' + rano.value + ' - Warranties - PCCG';
